2df2b66fda276e1a8b8c89702ddf9998dff7ea2b
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-03-07 12:59:23 +0200

    .

--- middleware/redirection.js
+++ middleware/redirection.js
@@ -1,12 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    console.log('Redirection Middleware Triggered');
+
     const response = await fetch('/api/geoip');
     const data = await response.json();
-    
-    // Unwrap the data correctly
+
+    console.log('GeoIP Data:', data); // Log the response to debug
+
     if (data && data.continent_code !== 'EU') {
+        console.log('Redirecting to mymonty.com.lb');
         return navigateTo('https://mymonty.com.lb', { 
             external: true,
             redirectCode: 302
-        })
+        });
     }
-})
+});
