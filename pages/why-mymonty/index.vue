00450ce0e3ede9a12fa8cbbdfcad6cb58b95f9a1
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-03-07 09:56:17 +0200

    Add new images and assets for the project

--- pages/why-mymonty/index.vue
+++ pages/why-mymonty/index.vue
@@ -28,19 +28,22 @@
                     <NuxtImg loading="lazy" class="w-full mt-8" src="/images/recurring-payments.webp" :alt="t(`Pages.Why MyMonty.Section 2.Cards[1].Image Alt`)" width="1852" height="832" />
                 </div> -->
                 <div class="grid lg:grid-cols-2 gap-4">
-                    <div class="p-8 lg:p-12 bg-[#F7F7F7] rounded-[30px]">
+                    <div class="flex flex-col gap-8 justify-between p-8 lg:p-12 bg-[#F7F7F7] rounded-[30px]">
                         <div class="flex flex-col justify-center max-lg:items-center gap-4 lg:gap-8 max-lg:mx-auto max-lg:text-center">
                             <h2 class="font-AeonikBlack text-3xl md:text-4xl xl:text-5xl 4xl:text-6xl uppercase">{{ t(`Pages.Why MyMonty.Section 2.Cards[2].Title`) }}</h2>
                             <p class="text-xl" v-html="t(`Pages.Why MyMonty.Section 2.Cards[2].Description`)"></p>
                         </div>
-                        <NuxtImg loading="lazy" class="w-full mt-8" src="/images/no-surprise-fees.webp" :alt="t(`Pages.Why MyMonty.Section 2.Cards[2].Image Alt`)" width="924" height="928" />
+                        <NuxtImg loading="lazy" class="w-full" src="/images/no-surprise-fees.webp" :alt="t(`Pages.Why MyMonty.Section 2.Cards[2].Image Alt`)" width="880" height="880" />
                     </div>
-                    <div class="p-8 lg:p-12 bg-[#F7F7F7] rounded-[30px]">
+                    <div class="flex flex-col gap-8 justify-between p-8 lg:p-12 bg-[#F7F7F7] rounded-[30px]">
                         <div class="flex flex-col justify-center max-lg:items-center gap-4 lg:gap-8 max-lg:mx-auto max-lg:text-center">
                             <h2 class="font-AeonikBlack text-3xl md:text-4xl xl:text-5xl 4xl:text-6xl uppercase">{{ t(`Pages.Why MyMonty.Section 2.Cards[3].Title`) }}</h2>
                             <p class="text-xl" v-html="t(`Pages.Why MyMonty.Section 2.Cards[3].Description`)"></p>
                         </div>
-                        <NuxtImg loading="lazy" class="w-full mt-8" src="/images/here-for-you.webp" :alt="t(`Pages.Why MyMonty.Section 2.Cards[3].Image Alt`)" width="924" height="928" />
+                        <div class="relative">
+                            <NuxtImg loading="lazy" class="w-full" src="/images/here-for-you.webp" :alt="t(`Pages.Why MyMonty.Section 2.Cards[3].Image Alt`)" width="880" height="880" />
+                            <NuxtImg loading="lazy" class="w-[160px] absolute -top-6 -left-6" src="/images/here-for-you-chat.webp" :alt="t(`Pages.Why MyMonty.Section 2.Cards[3].Image Alt`)" width="152" height="113" />
+                        </div>
                     </div>
                 </div>
                 <div class="p-8 lg:p-12 bg-[#F7F7F7] rounded-[30px]">
