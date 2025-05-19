59455a2c9686676fffd61daf58a6cb27b288d6c9
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-05-19 13:44:41 +0300

    .

--- server/api/geoip.js
+++ server/api/geoip.js
@@ -8,7 +8,8 @@ export default defineEventHandler(async (event) => {
     const testIp3 = '101.56.0.1'; // Replace with an IP you want to test (Italy)
     const testIp4 = '109.120.192.1'; // Replace with an IP you want to test (Bulgaria)
     const testIp5 = '178.238.11.6'; // Replace with an IP you want to test (United Kingdom)
-    
+    const testIp6 = '185.93.180.131'; // Replace with an IP you want to test (Frankfurt, Germany)
+
     // const ip = process.env.NODE_ENV === 'development' ? testIp : getRequestHeader(event, 'x-forwarded-for') || event.node.req.connection.remoteAddress || '127.0.0.1';
     const ip = process.env.NODE_ENV === 'development' ? testIp : getRequestHeader(event, 'x-forwarded-for')?.split(',')[0].trim() || event.node.req.socket.remoteAddress || '127.0.0.1';
     // const forwarded = getRequestHeader(event, 'x-forwarded-for');
@@ -17,24 +18,23 @@ export default defineEventHandler(async (event) => {
     // Clean IPv6 localhost
     const cleanedIp = ip === '::1' ? '127.0.0.1' : ip;
 
-    const geoDbPath = path.resolve('server/geoip/GeoLite2-Country1.mmdb');
+    const geoDbPath = path.resolve('server/geoip/GeoLite2-Country-new.mmdb');
 
     try {
         const lookup = await maxmind.open(geoDbPath);
-        const geoData = lookup.get(cleanedIp);
+        const geoData = lookup.get(testIp6);
 
         if (geoData && geoData.country) {
             return { 
                 ip: cleanedIp,
                 country: geoData.country.iso_code,
                 continent_code: geoData.continent.code,
-                flag: 'DETECTED'
             };
         }
 
-        return { ip: cleanedIp, country: 'LB', continent_code: 'AS', flag: 'NOT DETECTED' }; // Default to Lebanon if detection fails
+        return { ip: cleanedIp, country: 'LB', continent_code: 'AS'}; // Default to Lebanon if detection fails
     } catch (error) {
         console.error('Error accessing GeoLite2 database:', error);
-        return { error: error, cleanedIp: cleanedIp, country: 'LB', continent_code: 'AS' }; // Default to Lebanon in case of error
+        return { country: 'LB', continent_code: 'AS' }; // Default to Lebanon in case of error
     }
 });
